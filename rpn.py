6372cd522b5386ccfb70154c19b6aa22d484f298
michelle <michzhou@umich.edu>
2018-11-07 13:12:26 -0500

    added history fxnality

--- rpn.py
+++ rpn.py
@@ -13,31 +13,53 @@ operators = {
     '!': math.factorial,
 }
 
-def calculate(myarg):
+def calculate(myarg, hist):
     stack = list()
-    for token in myarg.split():
-        try:
-            token = int(token)
-            stack.append(token)
-        except ValueError:
-            function = operators[token]
-            if token != '!':
-                arg2 = stack.pop()
-                arg1 = stack.pop()
-                result = function(arg1, arg2)
-                stack.append(result)
-            else:
-                arg1 = stack.pop()
-                result = function(arg1)
-                stack.append(result)
-        print(stack)
+    if myarg == "history":
+        answer = hist.pop()
+        op = hist.pop()
+        if op == '!':
+            arg1  = hist.pop()
+            
+            return "{arg1} ! = {answer}".format(answer=answer, arg1=arg1)
+        else:
+            arg2 = hist.pop()
+            arg1 = hist.pop()
+
+            return "{arg1} {op} {arg2} = {answer}".format(arg1=arg1, op=op, arg2=arg2, answer=answer)
+    else:
+        for token in myarg.split():
+            try:
+                token = int(token)
+                stack.append(token)
+            except ValueError:
+                function = operators[token]
+                if token != '!':
+                    arg2 = stack.pop()
+                    arg1 = stack.pop()
+                    hist.append(arg1)
+                    hist.append(arg2)
+                    hist.append(token)
+                    result = function(arg1, arg2)
+                    stack.append(result)
+                    hist.append(result)
+                else:
+                    arg1 = stack.pop()
+                    hist.append(arg1)
+                    hist.append(token)
+                    result = function(arg1)
+                    stack.append(result)
+                    hist.append(result)
+                print(stack)
     if len(stack) != 1:
         raise TypeError("Too many parameters")
     return stack.pop()
 
 def main():
+    hist = list()
+    
     while True:
-        result = calculate(input("rpn calc> "))
+        result = calculate(input("rpn calc> "), hist)
         print("Result: ", result)
 
 if __name__ == '__main__':
