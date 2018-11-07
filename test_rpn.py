8a8ed82326a0c5cfa402213796544b99543ca92f
michelle <michzhou@umich.edu>
2018-11-07 14:58:34 -0500

    fixed history tests

--- test_rpn.py
+++ test_rpn.py
@@ -2,32 +2,39 @@ import unittest
 
 import rpn
 
-class TestBasics(unittest.TestCase):
+class TestBasics(unittest.TestCase):    
     def test_add(self):
-        result = rpn.calculate("1 1 +")
+        hist = list()
+        result = rpn.calculate("1 1 +", hist)
         self.assertEqual(2, result)
     def test_subtract(self):
-        result = rpn.calculate("5 3 -")
+        hist = list()
+        result = rpn.calculate("5 3 -", hist)
         self.assertEqual(2, result)
     def test_multiply(self):
-        result = rpn.calculate("5 3 *")
+        hist = list()
+        result = rpn.calculate("5 3 *", hist)
         self.assertEqual(15, result)
     def test_divide(self):
-        result = rpn.calculate("6 3 /")
+        hist = list()
+        result = rpn.calculate("6 3 /", hist)
         self.assertEqual(2, result)
     def test_exponent(self):
-        result = rpn.calculate("2 3 ^")
+        hist = list()
+        result = rpn.calculate("2 3 ^", hist)
         self.assertEqual(8, result)
     def test_factorial(self):
-        result = rpn.calculate("4 !")
+        hist = list()
+        result = rpn.calculate("4 !", hist)
         self.assertEqual(24, result)
     def test_history(self):
-        calculation = rpn.calculate("2 3 +")
-        result = rpn.calculate("history")
-        self.assertEqual("2 + 3 = 5")
-        calculation = rpn.calculate("4 !")
-        result = rpn.calculate("history")
-        self.assertEqual("4 ! = 24")
+        hist = list()
+        calculation = rpn.calculate("2 3 +", hist)
+        result = rpn.calculate("history", hist)
+        self.assertEqual("2 + 3 = 5", result)
+        calculation = rpn.calculate("4 !", hist)
+        result = rpn.calculate("history", hist)
+        self.assertEqual("4 ! = 24", result)
 
 if __name__ == "__main__":
    unittest.main()
